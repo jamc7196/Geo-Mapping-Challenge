c9ee0be8a445255410d0abb53f74634a4bc857a6
unknown <jamc7186@gmail.com>
2020-06-26 20:58:40 -0500

    final adjustment

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-//student: Jorge Alberto Muñozcano Castro
+//Student: Jorge Alberto Muñozcano Castro
 // Leafleft-Challenge
 //1) define variable with the usgs url
 var EQUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
@@ -7,7 +7,7 @@ d3.json(EQUrl, function(data) {
   createFeatures(data.features);
   console.log(data.features)
 });
-// Used a feature function where you give proper format to the data extract from json url
+// 3)Used a feature function where you give proper format to the data extract from json url
 function createFeatures(EQData) {
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
@@ -39,7 +39,6 @@ function createFeatures(EQData) {
       return "red"
     }
   }
-
 //5) Estblished an earthque variable where you store the data and the latitude and longitude of the variables extract from the USG
   var earthquakesdata = L.geoJSON(EQData, {
     pointToLayer: function(EQData, latlng) {
@@ -51,10 +50,9 @@ function createFeatures(EQData) {
     },
     onEachFeature: onEachFeature
   });
-
   createMap(earthquakesdata);
 }
-
+// 6) Create a function and extract the 3 different maps using the mapbox api (Oudoors, Light and Satellite)
 function createMap(earthquakesdata) {
   var map1 = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -69,31 +67,25 @@ function createMap(earthquakesdata) {
     id: "mapbox/satellite-streets-v11",
     accessToken: API_KEY
   });
-
   var map3 = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
     id:"mapbox/light-v10",
     accessToken: API_KEY
   });
-
-  // Create the faultline layer
+  // 7)Create the faultline layer define a baseMaps object to hold the base layers over the map
   var faultline = new L.LayerGroup();
-  
-  // Define a baseMaps object to hold our base layers
   var EQMaps = {
     "Outdoor Map": map1,
     "Greyscale Map": map3,
     "Satellite Map": map2
   };
-
-  // Create overlay object to hold our overlay layer
+  // 8)Create overlay object to hold our overlay layer
   var overlayEQ = {
     Earthquakes: earthquakesdata,
     FaultLines: faultline
   };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // 9) Create the maps, giving it the streetmap and earthquakes layers to display 
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
@@ -101,18 +93,12 @@ function createMap(earthquakesdata) {
     zoom: 4,
     layers: [map1, earthquakesdata, faultline]
   });
-
-  // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
+  // 10) Create a layer control and  add the layer control to the map
   L.control.layers(EQMaps, overlayEQ, {
     collapsed: false
   }).addTo(myMap);
-
-  // Query to retrieve the faultline data
+  // 11)Query to retrieve the faultline data and create the faultlines over the map 
   var EQfaultline = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json";
-
-  // Create the faultlines and add them to the faultline layer
   d3.json(EQfaultline, function(data) {
     L.geoJSON(data, {
       style: function() {
@@ -120,8 +106,7 @@ function createMap(earthquakesdata) {
       }
     }).addTo(faultline)
   })
-
-  // color function to be used when creating the legend
+  // 12) color function to be used when creating the legend
   function getColor(d) {
     return d > 5 ? '#ff3333' : //red zone
            d > 4  ? '#ff9933' : //gold high risk
@@ -129,8 +114,8 @@ function createMap(earthquakesdata) {
            d > 2  ? '#33ff66' : // green  moderate risk
            d > 1  ? '#ccff33' : // blue low risk
                     '#ff33ff'; // purple safe zone
-  }
-// Add legend to the map
+  } 
+// 13) Add legend to the maps
   var maplegend = L.control({position: 'bottomright'}); 
   maplegend.onAdd = function (map) {
       var divlegend = L.DomUtil.create('div', 'info legend'),
@@ -141,9 +126,7 @@ function createMap(earthquakesdata) {
               '<i style="background:' + getColor(mags[i] + 1) + '"></i> ' +
               mags[i] + (mags[i + 1] ? '&ndash;' + mags[i + 1] + '<br>' : '+');
       }
-  
       return divlegend;
   };
-  
   maplegend.addTo(myMap);
 }
